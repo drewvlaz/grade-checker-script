629a0cabea2facbfb73d31b7451d62bf509add97
Drew Vlasnik <314dsv@gmail.com>
2019-01-14 11:19:21 -0500

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,1 +1,6 @@
-
+USERNAME = '' # student login credentials
+PASSWORD = ''
+URL = '' # site where grades are stored
+EMAIL_ADDRESS = '' # create a throw-away email because some security features have to be disabled to be accessed by yagmail
+EMAIL_PASSWORD = ''
+TARGET_ADDRESS = '' # the email you want to be notified by here (so you don't expose your actual email to security threats)
